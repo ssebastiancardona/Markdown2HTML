c89fee62b0d36e2beae3f7992293b299fdf37b52
ssebastiancardona <ssebastiancardona@gmail.com>
2022-01-02 16:14:25 -0500

    task 0

--- markdown2html.py
+++ markdown2html.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+""" Sintaxis markdown para 'facilitar la vida' """
+import sys
+import os
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
+        exit(1)
+    if not os.path.exists(sys.argv[1]):
+        sys.stderr.write("Missing " + sys.argv[1] + "\n")
+        exit(1)
